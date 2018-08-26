6fab32d44a776f3d164d6fbba02ed03e64f676da
Kanna99 <30925616+Kanna99@users.noreply.github.com>
2018-08-26 21:26:51 +0530

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/public/')).listen(8080, function () {
-    console.log('Tic-Tac-Toe running on 8080...');
-});
